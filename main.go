b9bd80df8aac85043755110761a0debc1e654a57
mstgnz <mesutgenez@hotmail.com>
2022-06-15 18:03:40 +0300

    The codes for getting the arguments and checking the file have been moved to the function.

--- main.go
+++ main.go
@@ -21,41 +21,7 @@ var (
 
 func main() {
 
-	// Get Flags
-	file := flag.String("file", "", "choose file path")
-	from := flag.String("from", "", "current file format (yaml-json-xml)")
-	to := flag.String("to", "", "convert file format (yaml-json-xml)")
-	flag.Parse()
-
-	// Check Flags
-	if len(*file) == 0 || len(*from) == 0 || len(*to) == 0 {
-		fmt.Printf("Usage:\t--file: choose file path\n\t--from: current file format (yaml-json-xml)\n\t--to: convert file format (yaml-json-xml)\n")
-		os.Exit(0)
-	}
-	// Info message
-	fmt.Printf("%v %v %v %v %v %v\n", color.RedString("This"), color.YellowString(*file), color.RedString("file are starting to convert from"), color.YellowString(*from), color.RedString("to"), color.YellowString(*to))
-
-	// Read File
-	by, err := ioutil.ReadFile(*file)
-	errorHandle(err)
-
-	switch *from {
-	case "json":
-		if !isJSON(by) {
-			fmt.Printf("You said you gave json but this file is not json.\nYour load file: %v", *file)
-			os.Exit(0)
-		}
-	case "yaml":
-		if !isYaml(by) {
-			fmt.Printf("You said you gave yaml but this file is not yaml.\nYour load file: %v", *file)
-			os.Exit(0)
-		}
-	case "xml":
-		if !isXml(by) {
-			fmt.Printf("You said you gave xml but this file is not xml.\nYour load file: %v", *file)
-			os.Exit(0)
-		}
-	}
+	getArgsAndCheckFile()
 
 	// Node
 	node = Linear()
@@ -103,6 +69,44 @@ func recursive1(typeArr []any) {
 			errorHandle(err)
 			node.AddToEnd(strconv.Itoa(key), nil, valFormat)
 			recursive(typeMap)
+		}
+	}
+}
+
+func getArgsAndCheckFile() {
+	// Get Flags
+	file := flag.String("file", "", "choose file path")
+	from := flag.String("from", "", "current file format (yaml-json-xml)")
+	to := flag.String("to", "", "convert file format (yaml-json-xml)")
+	flag.Parse()
+
+	// Check Flags
+	if len(*file) == 0 || len(*from) == 0 || len(*to) == 0 {
+		fmt.Printf("Usage:\t--file: choose file path\n\t--from: current file format (yaml-json-xml)\n\t--to: convert file format (yaml-json-xml)\n")
+		os.Exit(0)
+	}
+	// Info message
+	fmt.Printf("%v %v %v %v %v %v\n", color.RedString("This"), color.YellowString(*file), color.RedString("file are starting to convert from"), color.YellowString(*from), color.RedString("to"), color.YellowString(*to))
+
+	// Read File
+	by, err := ioutil.ReadFile(*file)
+	errorHandle(err)
+
+	switch *from {
+	case "json":
+		if !isJSON(by) {
+			fmt.Printf("You said you gave json but this file is not json.\nYour load file: %v", *file)
+			os.Exit(0)
+		}
+	case "yaml":
+		if !isYaml(by) {
+			fmt.Printf("You said you gave yaml but this file is not yaml.\nYour load file: %v", *file)
+			os.Exit(0)
+		}
+	case "xml":
+		if !isXml(by) {
+			fmt.Printf("You said you gave xml but this file is not xml.\nYour load file: %v", *file)
+			os.Exit(0)
 		}
 	}
 }
