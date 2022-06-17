4d2d004548cdd984fccfd2d88fabe63be48603f0
mstgnz <mesutgenez@hotmail.com>
2022-06-17 09:06:27 +0300

    json decode

--- node.go
+++ node.go
@@ -8,18 +8,18 @@ import (
 )
 
 type ILinear interface {
-	AddToStart(key string, value any, valueKind reflect.Type)
-	AddToAfter(key string, value any, valueKind reflect.Type, which string)
-	AddToEnd(key string, value any, valueKind reflect.Type)
-	Delete(Key string)
+	AddToStart(key string, value any)
+	AddToAfter(key string, value any, which string)
+	AddToEnd(key string, value any)
+	Delete(key string)
+	Exists() bool
 	Print()
 }
 
 type linear struct {
-	key       string
-	value     any
-	valueKind reflect.Type
-	next      *linear
+	key   string
+	value any
+	next  *linear
 }
 
 func Linear() ILinear {
@@ -27,23 +27,22 @@ func Linear() ILinear {
 }
 
 // AddToStart data
-func (node *linear) AddToStart(key string, value any, valueKind reflect.Type) {
+func (node *linear) AddToStart(key string, value any) {
 	temp := *node
 	node.key = key
 	node.value = value
-	node.valueKind = valueKind
 	node.next = &temp
 }
 
 // AddToAfter data
-func (node *linear) AddToAfter(key string, value any, valueKind reflect.Type, whichKey string) {
+func (node *linear) AddToAfter(key string, value any, whichKey string) {
 	iter := node
 	for iter.key != whichKey && iter.next != nil {
 		iter = iter.next
 	}
 	if iter.key == whichKey {
 		temp := *iter
-		iter.next = &linear{key: key, value: value, valueKind: valueKind, next: nil}
+		iter.next = &linear{key: key, value: value, next: nil}
 		iter.next.next = temp.next
 	} else {
 		fmt.Println(whichKey, "not found!")
@@ -51,17 +50,16 @@ func (node *linear) AddToAfter(key string, value any, valueKind reflect.Type, wh
 }
 
 // AddToEnd data
-func (node *linear) AddToEnd(key string, value any, valueKind reflect.Type) {
+func (node *linear) AddToEnd(key string, value any) {
 	iter := node
-	if iter.next == nil && len(iter.key) == 0 {
+	if iter.value == nil {
 		iter.key = key
 		iter.value = value
-		iter.valueKind = valueKind
 	} else {
 		for iter.next != nil {
 			iter = iter.next
 		}
-		iter.next = &linear{key: key, value: value, valueKind: valueKind, next: nil}
+		iter.next = &linear{key: key, value: value, next: nil}
 	}
 }
 
@@ -91,12 +89,16 @@ func (node *linear) Delete(key string) {
 // Print data
 func (node *linear) Print() {
 	iter := node
-	for iter != nil && len(iter.key) > 0 {
+	for iter.next != nil {
 		value := reflect.ValueOf(iter.value).String()
 		if reflect.TypeOf(iter.value) == nil {
 			value = reflect.Invalid.String()
 		}
-		fmt.Printf("Key: %v, Value: %v, ValueKind: %v\n", color.YellowString(iter.key), color.YellowString(value), color.YellowString(iter.valueKind.String()))
+		fmt.Printf("Key: %v, Value: %v\n", color.YellowString(iter.key), color.YellowString(value))
 		iter = iter.next
 	}
+}
+
+func (node *linear) Exists() bool {
+	return node != nil && node.value != nil
 }
--- xml.go
+++ xml.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"bytes"
 	"encoding/xml"
 )
 
+var (
+	xmlMap interface{}
+)
+
 func isXml(doc []byte) bool {
-	return xml.Unmarshal(doc, &typeMap) == nil
+	return xml.Unmarshal(doc, &xmlMap) == nil
+}
+
+// xmlReadByte just test code
+func xmlReadByte(doc []byte) error {
+
+	buf := bytes.NewBuffer(doc)
+	dec := xml.NewDecoder(buf)
+	err := dec.Decode(node)
+	if err != nil {
+		return err
+	}
+
+	//lessThan := byte('<') // 60
+	//greaterThan := byte('>') // 62
+	//slash := byte('/') // 47
+
+	/*for _, v := range doc {
+		if v != 9 && v != 10 && v != 32 {
+
+			fmt.Printf("%v %v\n", v, string(v))
+		}
+	}*/
+	//fmt.Println(string([]byte{114, 111, 111, 116}))
+	return nil
 }
