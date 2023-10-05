8b830a61db4a991a5a5971f6c7ae629c05bcf4e3
mstgnz <mesutgenez@hotmail.com>
2023-10-05 16:21:34 +0300

    xml

--- xml/xml.go
+++ xml/xml.go
@@ -2,6 +2,7 @@ package xml
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io"
 	"os"
 	"reflect"
@@ -34,12 +35,12 @@ func ReadXml(filename string) ([]byte, error) {
 // DecodeXml
 // Converts a byte array to a key value struct.
 func DecodeXml(byt []byte) (*node.Node, error) {
-	var (
-		Knot   *node.Node
-		Parent *node.Node
-		isNext bool
-	)
+
 	dec := xml.NewDecoder(strings.NewReader(string(byt)))
+
+	var Parent *node.Node
+	var Knot *node.Node
+	isNext := true
 
 	for {
 		t, err := dec.Token()
@@ -49,7 +50,7 @@ func DecodeXml(byt []byte) (*node.Node, error) {
 		if Knot == nil && reflect.TypeOf(t).Name() != "StartElement" {
 			continue
 		}
-		Knot = &node.Node{}
+
 		switch kind := t.(type) {
 		case xml.StartElement:
 			key := kind.Name.Local
@@ -72,7 +73,8 @@ func DecodeXml(byt []byte) (*node.Node, error) {
 				Knot.Value.Worth = val
 			} else {
 				Parent = Knot
-				Knot = Knot.AddToValue(Knot, node.Value{Node: &node.Node{Parent: Parent}})
+				Knot.Value.Node = &node.Node{Parent: Parent}
+				Knot = Knot.AddToValue(Knot, Knot.Value)
 			}
 		case xml.EndElement:
 			Parent = Knot.Parent
@@ -81,8 +83,38 @@ func DecodeXml(byt []byte) (*node.Node, error) {
 	}
 }
 
-// NodeToXml
-// TODO implement
-func NodeToXml(node *node.Node) ([]byte, error) {
-	return nil, nil
+// NodeToXml byte
+func NodeToXml(knot *node.Node) (string, error) {
+	var xmlBuilder strings.Builder
+	xmlBuilder.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>")
+	var generate func(node *node.Node)
+	generate = func(node *node.Node) {
+		for node != nil {
+			var attrBuilder strings.Builder
+			for k, v := range node.Value.Attr {
+				attrBuilder.WriteString(fmt.Sprintf(" %v=\"%v\"", k, v))
+			}
+			xmlBuilder.WriteString(fmt.Sprintf("<%v %v>%v</%v>", node.Key, attrBuilder, node.Value.Worth, node.Key))
+			// if Node Value.Node exists
+			if node.Value.Node != nil {
+				if len(node.Key) == 0 {
+					node.Key = "array"
+				}
+				generate(node.Value.Node)
+			}
+			// if Node Value.Array exists
+			if len(node.Value.Array) > 0 {
+				for _, slc := range node.Value.Array {
+					// if Array.Value.Node exists
+					if slc.Node != nil {
+						generate(slc.Node)
+					}
+				}
+			}
+			node = node.Next
+		}
+	}
+	generate(knot.Reset())
+
+	return xmlBuilder.String(), nil
 }
